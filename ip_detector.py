687e34f703009b4051ce762755755653a096d1d3
JungaStyle <nyurgustan23@gmail.com>
2026-06-18 19:42:09 +0900

    Requirments added

--- ip_detector.py
+++ ip_detector.py
@@ -3,37 +3,38 @@ from pprint import pprint
 
 
 class YandexDisk:
-    token = '' 
-    data = {'Authorization': f'OAuth {token}'}
+    def __init__(self, token):
+        self.headers = {'Authorization': f'OAuth {token}'}
 
-    def check_folder(folder):
+
+    def check_folder(self, folder):
             url = 'https://cloud-api.yandex.net/v1/disk/resources?path=' + folder
             try:
-                resp = requests.get(url, headers=YandexDisk.data)
+                resp = requests.get(url, headers=self.headers)
                 return resp.status_code
             
             except requests.exceptions.ConnectionError:
                 return print(f'Ошибка сети')
+
             
-    def create_folder(folder):
-        folder_code = YandexDisk.check_folder(folder)
+    def create_folder(self, folder):
+        folder_code = YandexDisk.check_folder(self, folder)
         if folder_code == 404:
             try:
                 url = 'https://cloud-api.yandex.net/v1/disk/resources?path=' + folder
-                requests.put(url, headers=YandexDisk.data) 
-
+                requests.put(url, headers=self.headers) 
             except requests.exceptions.ConnectionError:
                 return print(f'Ошибка сети')
 
-    def add_ip_info():
+    def add_ip_info(self):
         ip = IPfy.get_ip()
         ip_info = IPinfo.ip_info
-        YandexDisk.create_folder(ip)
+        YandexDisk.create_folder(self, ip)
         url = (
             f'https://cloud-api.yandex.net/v1/disk/resources/upload?path={ip}/{ip}_info.json&url={ip_info}'
         )
         try:
-            resp = requests.post(url, headers=YandexDisk.data)
+            resp = requests.post(url, headers=self.headers)
             if resp.status_code == 202:
                 print('Информация об IP успешно добавлена!')
             else:
@@ -52,8 +53,11 @@ class IPfy:
         return ip
 
 
+
 class IPinfo:
     ip = IPfy.get_ip()
     ip_info = 'https://ipinfo.io/' + ip + '/geo'
 
-YandexDisk.add_ip_info()
+
+person = YandexDisk('') #Нужно вставить сюда ваш токен
+person.add_ip_info()
